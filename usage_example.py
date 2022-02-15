765e1f64338abaa161acefaa1d039c5e18b4d9f0
Ilya Solozobov <i.solozobov@gmail.com>
2022-02-16 00:20:30 +0300

    rename files

--- usage_example.py
+++ usage_example.py
@@ -2,5 +2,5 @@ import cv2
 from table_extractor import extract_table
 
 
-image = cv2.imread('nonogramSolver/images/ 1.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('nonogramSolver/images/1.jpg', cv2.IMREAD_GRAYSCALE)
 table, vertical_header, horizontal_header = extract_table(image)
